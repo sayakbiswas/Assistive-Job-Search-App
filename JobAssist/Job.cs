be86676eed69025833e1360f956cbfbb3e2e2a33
julianbrinkley <julianlbrinkley@gmail.com>
2016-10-23 14:44:27 -0500

    Initial Commit

---
+++ JobAssist/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobAssist
+{
+    public class Job
+    {
+        public string jobtitle { get; set; }
+        public string snippet { get; set; }
+
+    }
+}
